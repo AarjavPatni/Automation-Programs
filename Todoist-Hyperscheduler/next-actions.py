d7a9e4229ef14b76a2bd44dd1af3bc59e9f362b8
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2023-02-07 12:46:03 +0400

    Fix next-actions env variables

--- Todoist-Hyperscheduler/next-actions.py
+++ Todoist-Hyperscheduler/next-actions.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from todoist_api_python.api import TodoistAPI
 import os
+from dotenv import load_dotenv
 
-key = os.environ.get('todoist.env')
+load_dotenv(dotenv_path=r'C:\Users\Aarjav\Documents\Automation-Programs\Todoist-Hyperscheduler\todoist.env')
+
+key = os.environ.get('TODOIST_API_TOKEN')
 api = TodoistAPI(key)
 
 tasks = api.get_tasks(label='next-actions')
-print(tasks)
+for task in tasks:
+    print(task.content)
