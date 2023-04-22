31835f1d045660c28c5ab0d2cd1b5dd501d7cb8e
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2023-04-22 10:26:33 +0400

    Fix: only check for args if more than one

--- obsidian/reset-habit.py
+++ obsidian/reset-habit.py
@@ -26,41 +26,42 @@ for i in dirList:
 
 print('Done! New day tomorrow!')
 
-if args[1].lower() == 'def':
-    for k in dirList:
-        with open(k, "r", encoding="utf8") as f:
+if len(args) > 1:
+    if args[1].lower() == 'def':
+        for k in dirList:
+            with open(k, "r", encoding="utf8") as f:
+                try:
+                    content = f.read()
+                    write = True
+                except Exception:
+                    write = False
+
+            content = content.replace(
+                "- [x] Today", "- [ ] Today"
+            ).replace(
+                "- [x] 1 Day", "- [ ] 1 Day"
+            ).replace(
+                "- [x] 3 Days", "- [ ] 3 Days"
+            ).replace(
+                "- [x] 7 Days", "- [ ] 7 Days"
+            ).replace(
+                "- [x] 15 Days", "- [ ] 15 Days"
+            ).replace(
+                "- [x] 30 Days", "- [ ] 30 Days"
+            )
+
+            # Replace all lines that start with 'day-one: ' to 'day-one: today'
+            content = re.sub(r'^day-one:.*', 'day-one: today',
+                            content, flags=re.MULTILINE)
             try:
-                content = f.read()
-                write = True
-            except Exception:
-                write = False
+                # Replace all lines that start with 'skips-dates:' to 'skips-dates: ' until '---' is found
+                content = re.sub('skips-dates:(?s:.)*?(---)',
+                                'skips-dates:\n---', content, flags=re.MULTILINE)
+            except Exception as e:
+                print(e, k)
 
-        content = content.replace(
-            "- [x] Today", "- [ ] Today"
-        ).replace(
-            "- [x] 1 Day", "- [ ] 1 Day"
-        ).replace(
-            "- [x] 3 Days", "- [ ] 3 Days"
-        ).replace(
-            "- [x] 7 Days", "- [ ] 7 Days"
-        ).replace(
-            "- [x] 15 Days", "- [ ] 15 Days"
-        ).replace(
-            "- [x] 30 Days", "- [ ] 30 Days"
-        )
+            if write:
+                with open(k, "w", encoding="utf8") as f:
+                    f.write(content)
 
-        # Replace all lines that start with 'day-one: ' to 'day-one: today'
-        content = re.sub(r'^day-one:.*', 'day-one: today',
-                         content, flags=re.MULTILINE)
-        try:
-            # Replace all lines that start with 'skips-dates:' to 'skips-dates: ' until '---' is found
-            content = re.sub('skips-dates:(?s:.)*?(---)',
-                             'skips-dates:\n---', content, flags=re.MULTILINE)
-        except Exception as e:
-            print(e, k)
-
-        if write:
-            with open(k, "w", encoding="utf8") as f:
-                f.write(content)
-
-    print('Restored Defaults!')
+        print('Restored Defaults!')
