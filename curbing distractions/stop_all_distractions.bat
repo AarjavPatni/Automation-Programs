c6e64b246dfa1cb1d2f49dc28f9246650de232b0
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2024-03-30 04:14:02 -0400

    Remove unnecessary files

--- curbing distractions/stop_all_distractions.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "C:\Users\Aarjav\Documents\Automation Programs\curbing distractions\app_blocker.py"
--- discontinued/derivatives.py
+++
@@ -1,7 +0,0 @@
-import pyautogui as pg
-
-while True:
-    arg = input('Enter the arg: ')
-    pg.hotkey('alt', 'tab')
-    pg.sleep(1)
-    pg.write(f'\\[\\frac{{\\mathrm{{d}}}}{{\\mathrm{{d}} x}} ({arg}) =\\]')
